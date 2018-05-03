a2e31b20f85b6c37012c5c39114c8c87075167cf
JasonGuoCi <geqianci@163.com>
2018-05-03 15:37:29 +0800

    bundle home.js and easyui themes css in BundleConfig.cs

--- JProject/JProject.Mvc/App_Start/BundleConfig.cs
+++ JProject/JProject.Mvc/App_Start/BundleConfig.cs
@@ -14,6 +14,7 @@ namespace JProject.Mvc
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+
             // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
             // 生产准备就绪，请使用 https://modernizr.com 上的生成工具仅选择所需的测试。
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -23,9 +24,24 @@ namespace JProject.Mvc
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            //add home.js bundle
+            bundles.Add(new ScriptBundle("~/bundles/home").Include("~/Scripts/home.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryform").Include("~/Scripts/jquery.form.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/home").Include("~/Scripts/home.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            //add easy ui themes css
+            bundles.Add(new StyleBundle("~/Content/themes/blue/css").Include("~/Content/themes/blue/easyui.css"));
+            bundles.Add(new StyleBundle("~/Content/themes/gray/css").Include("~/Content/themes/gray/easyui.css"));
+            bundles.Add(new StyleBundle("~/Content/themes/metro/css").Include("~/Content/themes/metro/easyui.css"));
+            bundles.Add(new StyleBundle("~/Content/themes/black/css").Include("~/Content/themes/black/easyui.css"));
+            bundles.Add(new StyleBundle("~/Content/themes/bootstrap/css").Include("~/Content/themes/bootstrap/easyui.css"));
+            bundles.Add(new StyleBundle("~/Content/themes/default/css").Include("~/Content/themes/default/easyui.css"));
+            bundles.Add(new StyleBundle("~/Content/themes/material/css").Include("~/Content/themes/material/easyui.css"));
         }
     }
 }
