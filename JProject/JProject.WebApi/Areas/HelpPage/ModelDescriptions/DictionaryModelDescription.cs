c3f87e175ac5c38d4f46ed425d9276391d2d1247
JasonGuoCi <geqianci@163.com>
2018-05-07 16:08:21 +0800

    add Mvc.Account index视图 添加项目架构的解决方案

---
+++ JProject/JProject.WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace JProject.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ JProject/JProject.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JProject.WebApi.WebApiApplication" Language="C#" %>
