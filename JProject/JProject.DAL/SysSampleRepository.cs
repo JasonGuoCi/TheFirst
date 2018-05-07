6c1647ece5140d3c196402a5d7d1803c2763b9c6
JasonGuoCi <geqianci@163.com>
2018-05-07 17:44:03 +0800

    添加sysamplerepository DAL和IDAL结束
    Signed-off-by: JasonGuoCi <geqianci@163.com>

---
+++ JProject/JProject.DAL/SysSampleRepository.cs
@@ -0,0 +1,107 @@
+﻿using JProject.IDAL;
+using JProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JProject.DAL
+{
+    public class SysSampleRepository : ISysSampleRepository, IDisposable
+    {
+        /// <summary>
+        /// 创建数据实体
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public int Create(SysSample entity)
+        {
+            using (JProjectDBEntities db = new JProjectDBEntities())
+            {
+                db.Set<SysSample>().Add(entity);
+                return db.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// 根据ID删除数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public int Delete(string id)
+        {
+            using (JProjectDBEntities db = new JProjectDBEntities())
+            {
+                SysSample entity = db.SysSample.SingleOrDefault(a => a.Id == id);
+                db.Set<SysSample>().Remove(entity);
+                return db.SaveChanges();
+            }
+        }
+        /// <summary>
+        ///
+        /// </summary>
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// 修改数据
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public int Edit(SysSample entity)
+        {
+            using (JProjectDBEntities db = new JProjectDBEntities())
+            {
+                db.Set<SysSample>().Attach(entity);
+                db.Entry<SysSample>(entity).State = EntityState.Modified;
+                return db.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// 根据id获取数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public SysSample GetById(string id)
+        {
+            using (JProjectDBEntities db = new JProjectDBEntities())
+            {
+                return db.SysSample.SingleOrDefault(a => a.Id == id);
+            }
+        }
+        /// <summary>
+        /// 获取数据列表
+        /// </summary>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        public IQueryable<SysSample> GetList(JProjectDBEntities db)
+        {
+            IQueryable<SysSample> list = db.SysSample.AsQueryable();
+            return list;
+        }
+        /// <summary>
+        /// 根据id判断数据是否存在
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public bool IsExist(string id)
+        {
+            using (JProjectDBEntities db = new JProjectDBEntities())
+            {
+                SysSample entity = GetById(id);
+                if (entity != null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ JProject/JProject.IDAL/ISysSampleRepository.cs
@@ -0,0 +1,49 @@
+﻿using JProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JProject.IDAL
+{
+    public interface ISysSampleRepository
+    {
+        /// <summary>
+        /// 获取列表
+        /// </summary>
+        /// <param name="db">数据库上下文</param>
+        /// <returns>数据列表</returns>
+        IQueryable<SysSample> GetList(JProjectDBEntities db);
+        /// <summary>
+        /// 创建数据
+        /// </summary>
+        /// <param name="entity">实体</param>
+        /// <returns></returns>
+        int Create(SysSample entity);
+        /// <summary>
+        /// 删除数据
+        /// </summary>
+        /// <param name="id">数据ID</param>
+        /// <returns></returns>
+        int Delete(string id);
+        /// <summary>
+        /// 修改数据
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        int Edit(SysSample entity);
+        /// <summary>
+        /// 根据id获取数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        SysSample GetById(string id);
+        /// <summary>
+        ///根据ID判断数据是否存在
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        bool IsExist(string id);
+    }
+}
