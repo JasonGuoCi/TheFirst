c98a5c498c0ce0351016987902d0e85b67f08ae6
JasonGuoCi <geqianci@163.com>
2018-05-07 11:20:40 +0800

    生成导航栏js

--- JProject/JProject.Mvc/Scripts/home.js
+++ JProject/JProject.Mvc/Scripts/home.js
@@ -132,3 +132,28 @@ $(function () {
         });
     });
 });
+
+//生成导航栏
+$(function () {
+    var o = {
+        showcheck: false,
+        url: "/Home/GetTree",
+        onnodeclick: function (item) {
+            var tabTitle = item.text;
+            var url = "../../" + item.value;
+            var icon = item.Icon;
+            if (!item.hasChildren) {
+                addTab(tabTitle, url, icon);
+            } else {
+                $(this).parent().find("img").trigger("click");
+            }
+        }
+    }
+    $.post("/Home/GetTree", { "id": "0" }, function (data) {
+        if (data == "0") {
+            window.location = "/Account";
+        }
+        o.data = data;
+        $("#RightTree").treeview(o);
+    }, "json");
+});
